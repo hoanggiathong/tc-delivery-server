f4850b039b11e13e2a7ad95a0fc242a9847eb0f9
Leo <leonkenzo1997@gmail.com>
2025-11-30 14:47:58 +0700

    [fix]: api get-list-money-delivery-not-type-collect-cost-with-status-done wrong condition, change to createdAt to dateReturn

--- src/services/money-delivery.service.ts
+++ src/services/money-delivery.service.ts
@@ -1366,7 +1366,7 @@ export class MoneyDeliveryService {
         toRoute: toRouteId,
         status: MoneyDeliveryStatus.DONE,
         type: { $ne: MoneyDeliveryType.COLLECT },
-        createdAt: { $gte: startDate, $lte: endDate },
+        dateReturn: { $gte: startDate, $lte: endDate },
       })
         .populate([
           { path: 'sender', select: '_id name phone routeId createdAt updatedAt' },
@@ -1376,7 +1376,7 @@ export class MoneyDeliveryService {
           { path: 'createdByUser', select: '_id username' },
           { path: 'deliveryId', select: '_id code name createdAt updatedAt' },
         ])
-        .sort({ createdAt: -1 })
+        .sort({ dateReturn: -1 })
         .lean();
 
       const moneyDeliveriesResponse: IMoneyDeliveryResponse[] = moneyDeliveries.map(moneyDelivery =>
